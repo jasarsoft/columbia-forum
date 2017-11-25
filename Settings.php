3c118546050868f80fac852a3baccac14e4b63ec
Edin Jašarević <edinjasar14@gmail.com>
2017-11-25 23:16:21 +0100

    settings forum

--- Settings.php
+++ Settings.php
@@ -18,33 +18,34 @@ $mtitle = 'Maintenance Mode';		# Title for the Maintenance Mode message.
 $mmessage = 'Okay faithful users...we\'re attempting to restore an older backup of the database...news will be posted once we\'re back!';		# Description of why the forum is in maintenance mode.
 
 ########## Forum Info ##########
-$mbname = 'Columbia State RolePlay | Forum';		# The name of your forum.
+$mbname = 'Columbia State RolePlay';		# The name of your forum.
 $language = 'croatian-utf8';		# The default language file set for the forum.
-$boardurl = 'https://columbia-state.com/forum';		# URL to your forum's folder. (without the trailing /!)
+$boardurl = 'https://columbia-state.com';		# URL to your forum's folder. (without the trailing /!)
 $webmaster_email = 'board@columbia-state.com';		# Email address to send emails from. (like noreply@yourdomain.com.)
-$cookiename = 'SMFCookie248';		# Name of the cookie to set for authentication.
+$cookiename = 'SMFCookie549';		# Name of the cookie to set for authentication.
 
 ########## Database Info ##########
 $db_type = 'mysql';
 $db_server = 'localhost';
-$db_name = 'u0570120_smf805';
-$db_user = 'u0570120_smf805';
-$db_passwd = 'lQp5S)25]i';
+$db_name = 'u0570120_forum';
+$db_user = 'u0570120_forum';
+$db_passwd = 'S6[8Mq-8p5';
 $ssi_db_user = '';
 $ssi_db_passwd = '';
-$db_prefix = 'smfrc_';
+$db_prefix = 'forum_';
 $db_persist = 0;
-$db_error_send = 1;
+$db_error_send = 0;
 
 ########## Directories/Files ##########
 # Note: These directories do not have to be changed unless you move things.
-$boarddir = '/home/u0570120/public_html/forum';		# The absolute path to the forum's folder. (not just '.'!)
-$sourcedir = '/home/u0570120/public_html/forum/Sources';		# Path to the Sources directory.
-$cachedir = '/home/u0570120/public_html/forum/cache';		# Path to the cache directory.
+$boarddir = '/home/u0570120/public_html';		# The absolute path to the forum's folder. (not just '.'!)
+$sourcedir = '/home/u0570120/public_html/Sources';		# Path to the Sources directory.
+$cachedir = '/home/u0570120/public_html/cache';		# Path to the cache directory.
 
 ########## Error-Catching ##########
 # Note: You shouldn't touch these settings.
 $db_last_error = 0;
+
 
 # Make sure the paths are correct... at least try to fix them.
 if (!file_exists($boarddir) && file_exists(dirname(__FILE__) . '/agreement.txt'))
@@ -54,7 +55,7 @@ if (!file_exists($sourcedir) && file_exists($boarddir . '/Sources'))
 if (!file_exists($cachedir) && file_exists($boarddir . '/cache'))
 	$cachedir = $boarddir . '/cache';
 
-$image_proxy_secret = '05852d2f5c96e5cfd389';
+$image_proxy_secret = '4c14849eef04a96d78ae';
 $image_proxy_maxsize = '5190';
-$image_proxy_enabled = '0';
+$image_proxy_enabled = 0;
 ?>
--- Settings_bak.php
+++ Settings_bak.php
@@ -18,32 +18,44 @@ $mtitle = 'Maintenance Mode';		# Title for the Maintenance Mode message.
 $mmessage = 'Okay faithful users...we\'re attempting to restore an older backup of the database...news will be posted once we\'re back!';		# Description of why the forum is in maintenance mode.
 
 ########## Forum Info ##########
-$mbname = 'My Community';		# The name of your forum.
-$language = 'english';		# The default language file set for the forum.
-$boardurl = 'http://127.0.0.1/smf';		# URL to your forum's folder. (without the trailing /!)
-$webmaster_email = 'noreply@myserver.com';		# Email address to send emails from. (like noreply@yourdomain.com.)
-$cookiename = 'SMFCookie11';		# Name of the cookie to set for authentication.
+$mbname = 'Columbia State RolePlay';		# The name of your forum.
+$language = 'croatian-utf8';		# The default language file set for the forum.
+$boardurl = 'https://columbia-state.com';		# URL to your forum's folder. (without the trailing /!)
+$webmaster_email = 'board@columbia-state.com';		# Email address to send emails from. (like noreply@yourdomain.com.)
+$cookiename = 'SMFCookie549';		# Name of the cookie to set for authentication.
 
 ########## Database Info ##########
 $db_type = 'mysql';
 $db_server = 'localhost';
-$db_name = 'smf';
-$db_user = 'root';
-$db_passwd = '';
+$db_name = 'u0570120_forum';
+$db_user = 'u0570120_forum';
+$db_passwd = 'S6[8Mq-8p5';
 $ssi_db_user = '';
 $ssi_db_passwd = '';
-$db_prefix = 'smf_';
+$db_prefix = 'forum_';
 $db_persist = 0;
-$db_error_send = 1;
+$db_error_send = 0;
 
 ########## Directories/Files ##########
 # Note: These directories do not have to be changed unless you move things.
-$boarddir = dirname(__FILE__);		# The absolute path to the forum's folder. (not just '.'!)
-$sourcedir = dirname(__FILE__) . '/Sources';		# Path to the Sources directory.
-$cachedir = dirname(__FILE__) . '/cache';		# Path to the cache directory.
+$boarddir = '/home/u0570120/public_html';		# The absolute path to the forum's folder. (not just '.'!)
+$sourcedir = '/home/u0570120/public_html/Sources';		# Path to the Sources directory.
+$cachedir = '/home/u0570120/public_html/cache';		# Path to the cache directory.
 
 ########## Error-Catching ##########
 # Note: You shouldn't touch these settings.
 $db_last_error = 0;
 
+
+# Make sure the paths are correct... at least try to fix them.
+if (!file_exists($boarddir) && file_exists(dirname(__FILE__) . '/agreement.txt'))
+	$boarddir = dirname(__FILE__);
+if (!file_exists($sourcedir) && file_exists($boarddir . '/Sources'))
+	$sourcedir = $boarddir . '/Sources';
+if (!file_exists($cachedir) && file_exists($boarddir . '/cache'))
+	$cachedir = $boarddir . '/cache';
+
+$image_proxy_secret = '4c14849eef04a96d78ae';
+$image_proxy_maxsize = '5190';
+$image_proxy_enabled = 0;
 ?>
